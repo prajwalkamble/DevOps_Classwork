4e22685393490f4471502c3816d57b19981b0d88
Prajwal Kamble <65185064+prajwalkamble@users.noreply.github.com>
2024-03-01 11:28:38 +0530

    Update CarModel.java

--- CarModel.java
+++ CarModel.java
@@ -1,5 +1,5 @@
 class CarDetails{
-  String model, color
+  String model, color;
   public CarDetails(String model, String color){
     this.model = model;
     this.color = color;
@@ -29,9 +29,9 @@ class CarDetails{
 public class CarModel {
   public static void main(String[] args) {
     CarDetails car = new CarDetails("","");
-    car.setModel("Lamborghini");
-    car.setColor("White");
+    car.setModel("Lamborghini")
+    car.setColor("White")
 
-    car.display();    
+    car.display()
   }
 }
